e7e2151c4777166e315a8b54d7b920624491c25c
Arif Bepari <arifbepari2006@gmail.com>
2025-09-19 23:21:19 +0530

    rge

---
+++ shopping/src/pages/homepage.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import './homepage.css';
+import Diretory from "./directory";
+
+
+const Homepage = () => {
+    return (
+        <div className="homepage">
+            <Diretory />
+        </div>
+    )
+};
+
+export default Homepage;
